1918d6da920f60ae68db5dc56f42fe068954b007
Subasriseenivasan <144335218+Subasriseenivasan@users.noreply.github.com>
2023-10-04 14:14:18 +0530

    Update Class student.py

--- Class student.py
+++ Class student.py
@@ -1,1 +1,21 @@
+class Student:
+  def __init__(self, name, roll_number, cgpa):
 
+   self.name = name
+
+   self.roll_number = roll_number
+
+   self.cgpa = cgpa 
+
+def sort_students (student_list):
+
+# Sort the list of students in descending order of CGPA
+
+ sorted_students = sorted(student_list,
+
+key=lambda student: student.cgpa,
+
+reverse=True)
+
+# Syntax Lambda arg:exp return sorted_students
+ return sorted_students
